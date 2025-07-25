81b921339845f18036d356f09e3ee6052a8a623c
Joy12343 <jzhuo1919@gmail.com>
2025-07-25 17:01:33 -0400

    Clean up

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-input_dir = 'All_After_resized'
-output_dir = 'cleft_landmarks'
-txt_output_dir = 'cleft_landmarks_text'
-
-os.makedirs(output_dir, exist_ok=True)
-os.makedirs(txt_output_dir, exist_ok=True)
